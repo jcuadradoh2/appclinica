35a2b65fe14128bd16cc86bf4354a7c5a3bc18a4
ValeStef22 <vbunayp@unemi.edu.ec>
2020-10-03 19:21:14 -0500

    cambios en menu y paciente

--- appclinica/urls.py
+++ appclinica/urls.py
@@ -1,23 +1,9 @@
-"""appclinica URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+ 
 from django.contrib import admin
 from django.urls import path, include
 
 from login.views import LoginView, IndexView
-from appatencion.views import menuView
+from appatencion.views import menuView, opcionesView
 from django.conf import settings
 
 urlpatterns = [
@@ -25,10 +11,11 @@ urlpatterns = [
     path('', LoginView.as_view(), name='login'),    
     path('index/', IndexView.as_view(), name='index'),  
     path('menu/', menuView.as_view(), name='menu'),
+    path('opciones/', opcionesView.as_view(), name='opciones'),
+    path('base/', include('appcore.urls')),
 ]
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-
--- appcore/admin.py
+++ appcore/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from appcore.models import (Titulo, Profesion, Provincia, Paciente, Ciudad, Sangre)
+
+
+admin.site.register(Titulo)
+admin.site.register(Profesion)
+admin.site.register(Provincia)
+admin.site.register(Paciente)
+admin.site.register(Ciudad)
+admin.site.register(Sangre)
